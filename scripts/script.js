69ba3687f8ace793cffeec7dde5ae86523eff271
Nahid Hossain <oikantik@gmail.com>
2020-11-03 21:21:37 -0500

    added evente

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,55 @@
-let arr = {};
+let calculate = {
+  firstInput: [],
+  secondInput: [],
+  operator: null
+};
 
-console.log(arr);
+document.querySelectorAll('#number').forEach(element => {
+    element.addEventListener('click', (e)=> {
+        console.log(e.target.innerText);
+        // if first input is null, put a number
+        if(!calculate.operator) calculate.firstInput.push(e.target.innerText);
+        if(calculate.operator) calculate.secondInput.push(e.target.innerText)
+        console.log(calculate)
+
+    })
+})
+
+document.querySelectorAll('#operator').forEach(element => {
+  element.addEventListener('click', (e)=> {
+      console.log(e.target.innerText);
+      calculate.operator = e.target.innerText;
+  })
+})
+
+document.querySelector('#equal').addEventListener('click', (e)=> {
+
+  //convert string to number 
+  let firstInput = Number(calculate.firstInput.join(""));
+  let secondInput = Number(calculate.secondInput.join(""));
+  let result;
+  switch(calculate.operator) {
+    case '+': 
+      result = firstInput + secondInput;
+      break;
+
+      case '-': 
+      result = firstInput - secondInput;
+      break;
+
+      case 'x': 
+      result = firstInput * secondInput;
+      break;
+
+      case '÷': 
+      result = firstInput / secondInput;
+      break;
+
+    default:
+      break;
+  }
+  console.log(firstInput)
+  console.log(secondInput)
+  console.log(result)
+      //if(firstInput && secondInput && operator) 
+})
