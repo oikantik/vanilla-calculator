2647f8c10facdbb244ae4dca402dddf3c79a89f3
Nahid Hossain <oikantik@gmail.com>
2020-11-04 18:06:33 -0500

    added bare minimum javascript to make functions work

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,102 @@
+let firstNumber = document.querySelector("#first-number");
+let secondNumber = document.querySelector("#last-number");
+let resultNumber = document.querySelector("#result");
+let sign = document.querySelector("#sign");
+
 let calculate = {
   firstInput: [],
   secondInput: [],
-  operator: null
+  operator: null,
 };
 
-document.querySelectorAll('#number').forEach(element => {
-    element.addEventListener('click', (e)=> {
-        console.log(e.target.innerText);
-        // if first input is null, put a number
-        if(!calculate.operator) calculate.firstInput.push(e.target.innerText);
-        if(calculate.operator) calculate.secondInput.push(e.target.innerText)
-        console.log(calculate)
+const clearCalculate = () => {
+  calculate.firstInput = [];
+  calculate.secondInput = [];
+  calculate.operator = null;
+};
 
-    })
-})
+const clearResult = () => {
+  resultNumber.innerText = "";
+  firstNumber.innerText = "";
+  secondNumber.innerText = "";
+};
 
-document.querySelectorAll('#operator').forEach(element => {
-  element.addEventListener('click', (e)=> {
-      console.log(e.target.innerText);
-      calculate.operator = e.target.innerText;
-  })
-})
-
-document.querySelector('#equal').addEventListener('click', (e)=> {
-
-  //convert string to number 
-  let firstInput = Number(calculate.firstInput.join(""));
-  let secondInput = Number(calculate.secondInput.join(""));
+const calculateNumbers = (calculate, firstInput, secondInput) => {
   let result;
-  switch(calculate.operator) {
-    case '+': 
+  switch (calculate.operator) {
+    case "+":
       result = firstInput + secondInput;
+      console.log(result);
+      resultNumber.innerText = result;
       break;
 
-      case '-': 
+    case "-":
       result = firstInput - secondInput;
+      resultNumber.innerText = result;
       break;
 
-      case 'x': 
+    case "x":
       result = firstInput * secondInput;
+      resultNumber.innerText = result;
       break;
 
-      case '÷': 
+    case "÷":
       result = firstInput / secondInput;
+      resultNumber.innerText = result;
       break;
 
     default:
       break;
   }
-  console.log(firstInput)
-  console.log(secondInput)
-  console.log(result)
-      //if(firstInput && secondInput && operator) 
+}
+
+document.querySelectorAll("#number").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    console.log(e.target.innerText);
+    // if first input is null, put a number
+    if (!calculate.operator) {
+      clearResult();
+      calculate.firstInput.push(e.target.innerText);
+      firstNumber.innerText = calculate.firstInput.join("");
+    }
+    if (calculate.operator) {
+      calculate.secondInput.push(e.target.innerText);
+      secondNumber.innerText = calculate.secondInput.join("");
+    }
+    console.log(calculate);
+  });
+});
+
+document.querySelectorAll("#operator").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    console.log(e.target.innerText);
+    calculate.operator = e.target.innerText;
+    sign.innerText = calculate.operator;
+  });
+});
+
+document.querySelector("#equal").addEventListener("click", (e) => {
+  //convert string to number
+  let firstInput = Number(calculate.firstInput.join(""));
+  let secondInput = Number(calculate.secondInput.join(""));
+  calculateNumbers(calculate, firstInput, secondInput);
+  console.log(firstInput);
+  console.log(secondInput);
+  //if(firstInput && secondInput && operator)
+});
+
+document.querySelector('#percentage').addEventListener('click', () => {
+  let firstInput = Number(calculate.firstInput.join(""));
+  let secondInput = (Number(calculate.secondInput.join("")) /100) * firstInput;
+  calculateNumbers(calculate, firstInput, secondInput);
+  console.log(calculate)
 })
+
+document.querySelector('#plusMinus').addEventListener('click', () => {
+  if(calculate.secondInput !== '-') calculate.secondInput.unshift('-');
+})
+
+document.querySelector("#clear").addEventListener("click", () => {
+  clearCalculate();
+  clearResult();
+});
